83177d27c5c98e126958caaf65763d26f036260d
Micah Cheng <heymicahme@gmail.com>
2022-01-25 20:22:13 -0500

    Organize into folders

---
+++ src/rewrite.py
@@ -0,0 +1,15 @@
+import datetime
+
+x = datetime.datetime(2021, 6, 19)
+
+puzzleNumber = 1
+with open("../data/answers.txt", 'r') as answerWords:
+	words = answerWords.readlines()[0].strip().split(" ")
+with open('../data/searchableAnswers.txt', 'w') as outfile:
+	for word in words:
+		outfile.write(str(puzzleNumber) + ": ")
+		outfile.write(x.strftime("%B %d, %Y "))
+		outfile.write(word)
+		outfile.write('\n')
+		x += datetime.timedelta(days=1)
+		puzzleNumber += 1
